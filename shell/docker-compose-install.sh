e2fc12a959d83bd01bfd31b1dd5559635440c9ed
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-02-09 15:46:20 +0800

    Create docker-compose-install.sh

---
+++ shell/docker-compose-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "输入1自定义安装，2默认docker-ce-18.06.1.ce-3.el7安装"
+read -p "请输入:" select_docker_version
+if [ $select_docker_version == 1 ]; then
+        read -p "docker_version版本:" docker_version
+        yum install -y yum-utils && \
+        yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo && \
+        yum -y install $docker_version && \
+        systemctl start docker && \
+        systemctl enable docker
+        if [ $? -eq 0 ]; then
+                echo "$docker_version 安装完成，启动完成并开启开机自启"
+        else
+                echo "$docker_version 安装失败"
+        echo "安装docker-compose version 1.18.0"
+        curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+        chmod +x /usr/local/bin/docker-compose && \
+        ln -s /usr/local/bin/docker-compose /usr/bin/
+        if [ $? -eq 0 ]; then
+                echo "docker-compose version 1.18.0 安装完成，已内置系统命令"
+        else
+                echo "docker-compose version 1.18.0安装失败"
+elif [ $select_docker_version == 2]; then
+        yum install -y yum-utils && \
+        yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo && \
+        yum -y install docker-ce-18.06.1.ce-3.el7 && \
+        systemctl start docker && \
+        systemctl enable docker
+        if [ $? -eq 0 ]; then
+                echo "docker-ce-18.06.1.ce-3.el7 安装完成，启动完成并开启开机自启"
+        else
+                echo "docker-ce-18.06.1.ce-3.el7 安装失败"
+        echo "安装docker-compose version 1.18.0"
+        curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+        chmod +x /usr/local/bin/docker-compose && \
+        ln -s /usr/local/bin/docker-compose /usr/bin/
+        if [ $? -eq 0 ]; then
+                echo "docker-compose version 1.18.0 安装完成，已内置系统命令"
+        else
+                echo "docker-compose version 1.18.0安装失败"
+else
+        echo "请输入1或2"
