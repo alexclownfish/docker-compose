ef8e9791f05d062509691f5136f939e4d67a5286
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-03-03 13:37:27 +0800

    Update network.sh

--- shell/network.sh
+++ shell/network.sh
@@ -1,3 +1,12 @@
 docker network create --driver bridge --subnet 172.62.0.0/16 --gateway 172.62.0.1 cq-data-security
-docker network ls | grep cq-data-security
-echo -e "\e[35m docker-compose version 1.18.0 cq-data-security 网格创建完成，执行docker-compose up -d \e[0m"
+
+IsNetworkOk=`docker network ls | grep cq-data-security | awk {'print $2'}`
+
+sleep 2
+
+if [ $IsNetworkOk == 'cq-data-security' ] 
+then 
+	echo -e "\e[35m  cq-data-security 网格创建完成，执行docker-compose up -d启动项目 \e[0m"
+else
+	echo -e "\e[31m  cq-data-security 网格创建失败 \e[0m"
+fi
